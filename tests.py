3575d5a00ee1158b9b19471561aa9ad14a2cd894
Phondanai Khanti <phondanai@gmail.com>
2020-03-31 12:05:51 +0700

    Add simple test

---
+++ tests.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+import unittest
+
+from app import create_app, db
+from app.models import Carpark
+from config import Config
+
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
+
+
+class CarparkModelsTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = create_app(TestConfig)
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_create_data_record(self):
+        now = datetime.utcnow()
+        car = Carpark(floor_slot='F2_3', available=False, timestamp=now)
+        db.session.add(car)
+        db.session.commit()
+        self.assertEqual(car.floor_slot, 'F2_3')
+        self.assertFalse(car.available)
+        self.assertEqual(car.timestamp, now)
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
