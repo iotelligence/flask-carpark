83f51bef58e03705f89f8bdba17a9f2660c0a8aa
Max Chuang <hwesun@gmail.com>
2020-04-07 16:23:31 +0700

    add comments to tests.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MQTT_BROKER_URL = os.environ.get('MQTT_BROKER_URL') or 'mqtt.netpie.io'
     MQTT_BROKER_PORT = os.environ.get('MQTT_BROKER_PORT') or 1883
-    MQTT_CLIENT_ID = os.environ.get('MQTT_CLIENT_ID') or None
-    MQTT_TOKEN = os.environ.get('MQTT_TOKEN') or None
+    MQTT_CLIENT_ID = os.environ.get('MQTT_CLIENT_ID') or 'c9e27032-f75b-4fed-893c-92c199f5e491'
+    MQTT_TOKEN = os.environ.get('MQTT_TOKEN') or 'T6(T3JvAGS&!ygw*v(t.X08@c0vm&JiS'
     UPLOAD_FOLDER = os.path.join(basedir, 'app/static/csv')
     MAX_CONTENT_LENGTH = 2 * 1024 * 1024
--- tests.py
+++ tests.py
@@ -1,15 +1,14 @@
 from datetime import datetime
 import unittest
-import csv
-import sqlite3
+
 
 from app import create_app, db
 from app.models import Carpark
 from config import Config
 
-
 class TestConfig(Config):
     TESTING = True
+    #using sqlite database for testing
     SQLALCHEMY_DATABASE_URI = "sqlite://"
 
 
@@ -19,18 +18,23 @@ class CarparkModelsTestCase(unittest.TestCase):
     """
 
     def setUp(self):
+        #start a temp service
         self.app = create_app(TestConfig)
         self.app_context = self.app.app_context()
         self.app_context.push()
+        #create a testing database
         db.create_all()
 
     def tearDown(self):
+        #finish testing, drop the database
         db.session.remove()
         db.drop_all()
         self.app_context.pop()
 
     def test_create_data_record(self):
+        #getting the current time
         now = datetime.utcnow()
+        #import the data by Carpark function
         car = Carpark(floor_slot="F2_3", available=False, timestamp=now)
         db.session.add(car)
         db.session.commit()
@@ -53,21 +57,28 @@ class DashboardPageTestCase(unittest.TestCase):
     """
 
     def setUp(self):
+        #start a temp service
         self.app = create_app(TestConfig)
         self.app_context = self.app.app_context()
         self.app_context.push()
         self.client = self.app.test_client()
+        #create a testing database
         db.create_all()
 
     def tearDown(self):
+        #finish testing, drop the database
         db.session.remove()
         db.drop_all()
         self.app_context.pop()
 
     def test_dashboard_status_code(self):
+        #get the response of the url https://servername/
         result = self.client.get("/")
+        #check if the response status code is 200 or not
         self.assertEqual(result.status_code, 200)
+        #get the response of the url https://servername/dashboard 
         result = self.client.get("/dashboard")
+        #check if the response status code is 200 or not
         self.assertEqual(result.status_code, 200)
 
 
@@ -77,17 +88,21 @@ class ExportDataTestcase(unittest.TestCase):
     """
 
     def setUp(self):
+        #start a temp service
         self.app = create_app(TestConfig)
         self.app_context = self.app.app_context()
         self.app_context.push()
         self.client = self.app.test_client()
+        #create a testing database
         db.create_all()
-
+        
+        #import the data to the sqlite database
         now = datetime.utcnow()
         car = Carpark(floor_slot="F2_3", available=False, timestamp=now)
         db.session.add(car)
         db.session.commit()
-
+        
+        #transfer the data from each column to csv format
         floor_slot = car.floor_slot
         available = "available" if car.available == True else "occupied"
         date = car.timestamp.strftime("%Y-%m-%d")
@@ -97,28 +112,29 @@ class ExportDataTestcase(unittest.TestCase):
         ).encode("utf-8")
 
     def tearDown(self):
+        #finish testing, drop the database
         db.session.remove()
         db.drop_all()
         self.app_context.pop()
 
     def test_export_status_code(self):
+        #get the response of the url https://servername/export
         result = self.client.get("/export")
+        #check if the response status code is 200 or not
         self.assertEqual(result.status_code, 200)
 
     def test_export_mimetype_csv(self):
+        #get the response of the url https://servername/export
         result = self.client.get("/export")
+        #check if the export file is in csv format
         self.assertEqual(result.mimetype, "text/csv")
 
     def test_export_csv_data(self):
+        #get the response of the url https://servername/export
         result = self.client.get("/export")
+        #check if the export file's data matchs with the import string
         self.assertEqual(result.data, self.expect)
-        
-    def test_check_csv_data(self) :
-        result = self.client.get("/export")
-        rows = csv.reader(Result)
-        for row in rows:
-            print(row)
-        print(self.expect)        
+                
 
 class InsightTestcase(unittest.TestCase):
     """
@@ -126,19 +142,24 @@ class InsightTestcase(unittest.TestCase):
     """
 
     def setUp(self):
+        #start a temp service
         self.app = create_app(TestConfig)
         self.app_context = self.app.app_context()
         self.app_context.push()
         self.client = self.app.test_client()
+        #create a testing database
         db.create_all()
 
     def tearDown(self):
+        #finsh testing, drop the database
         db.session.remove()
         db.drop_all()
         self.app_context.pop()
 
     def test_insights_status_code(self):
+        #get the response of the url https://servername/insights
         result = self.client.get("/insights")
+        #check if the response status code is 200 or not
         self.assertEqual(result.status_code, 200)
 
 
