94d470338ad8853cf168d13bc5b0ee47428a2030
Phondanai Khanti <phondanai@gmail.com>
2020-04-07 11:28:18 +0700

    Remove all floor display table

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -8,10 +8,30 @@ $(document).ready(function(){
     // event handler for server sent data
     // the data is displayed in the "Received" section of the page
     socket.on('my connected', function(msg) {
-        avail = $("tr[id*='tr_'] td:contains(available)").length;
-        occupied= $("tr[id*='tr_'] td:contains(occupied)").length;
+        avail = $("tr[id*='trs_'] td:contains(available)").length;
+        occupied= $("tr[id*='trs_'] td:contains(occupied)").length;
         $('#log').html(msg.data);
         $('#avail').html('Available: '+ avail + ',' + 'Occupied: ' + occupied);
+
+        f1_avail = $("tr[id*='trs_F1_'] td:contains(available)").length;
+        f1_occ = $("tr[id*='trs_F1_'] td:contains(occupied)").length;
+        $('#F1_avail').html(f1_avail);
+        $('#F1_occ').html(f1_occ);
+
+        f2_avail = $("tr[id*='trs_F2_'] td:contains(available)").length;
+        f2_occ = $("tr[id*='trs_F2_'] td:contains(occupied)").length;
+        $('#F2_avail').html(f2_avail);
+        $('#F2_occ').html(f2_occ);
+
+        f3_avail = $("tr[id*='trs_F3_'] td:contains(available)").length;
+        f3_occ = $("tr[id*='trs_F3_'] td:contains(occupied)").length;
+        $('#F3_avail').html(f1_avail);
+        $('#F3_occ').html(f1_occ);
+
+        f4_avail = $("tr[id*='trs_F4_'] td:contains(available)").length;
+        f4_occ = $("tr[id*='trs_F4_'] td:contains(occupied)").length;
+        $('#F4_avail').html(f1_avail);
+        $('#F4_occ').html(f1_occ);
     });
 
     socket.on('my response', function(msg) {
@@ -27,9 +47,29 @@ $(document).ready(function(){
             $('#trs_'+msg.floor).html('<td>'+msg.floor+'</td>'+'<td bgcolor="#FA7268">'+msg.avail+'</td>'+'<td>'+msg.last_update+'</td>').fadeTo(100, 0.3, function() { $(this).fadeTo(300, 1.0);});
         }
 
-        avail = $("tr[id*='tr_'] td:contains(available)").length;
-        occupied= $("tr[id*='tr_'] td:contains(occupied)").length;
+        avail = $("tr[id*='trs_'] td:contains(available)").length;
+        occupied= $("tr[id*='trs_'] td:contains(occupied)").length;
         $('#avail').html('Available: '+ avail + ',' + 'Occupied: ' + occupied);
+
+        f1_avail = $("tr[id*='trs_F1_'] td:contains(available)").length;
+        f1_occ = $("tr[id*='trs_F1_'] td:contains(occupied)").length;
+        $('#F1_avail').html(f1_avail);
+        $('#F1_occ').html(f1_occ);
+
+        f2_avail = $("tr[id*='trs_F2_'] td:contains(available)").length;
+        f2_occ = $("tr[id*='trs_F2_'] td:contains(occupied)").length;
+        $('#F2_avail').html(f2_avail);
+        $('#F2_occ').html(f2_occ);
+
+        f3_avail = $("tr[id*='trs_F3_'] td:contains(available)").length;
+        f3_occ = $("tr[id*='trs_F3_'] td:contains(occupied)").length;
+        $('#F3_avail').html(f1_avail);
+        $('#F3_occ').html(f1_occ);
+
+        f4_avail = $("tr[id*='trs_F4_'] td:contains(available)").length;
+        f4_occ = $("tr[id*='trs_F4_'] td:contains(occupied)").length;
+        $('#F4_avail').html(f1_avail);
+        $('#F4_occ').html(f1_occ);
     });
 
     // event handler for new connections
@@ -68,13 +108,3 @@ $(document).ready(function(){
         return false;
     });
 });
-
-
-function hide() {
-    var x = document.getElementById("all_floor");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
